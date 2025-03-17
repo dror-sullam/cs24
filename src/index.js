180b620a85de837ffa72185a2b18d10b444c1957
daniel-ziv <selerbi3@gmail.com>
2025-03-17 10:36:42 +0200

    deleted some files

--- src/index.js
+++ src/index.js
@@ -2,31 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRoutes from './Routes';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-// Get the Google Client ID from environment variables
-const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-
-// Make sure we have a client ID
-if (!googleClientId) {
-  console.error('Missing Google Client ID. Please check your environment variables.');
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider 
-      clientId={googleClientId}
-      onScriptLoadError={() => {
-        console.error('Failed to load Google OAuth script');
-      }}
-      onScriptLoadSuccess={() => {
-        console.log('Google OAuth script loaded successfully');
-      }}
-    >
-      <AppRoutes />
-    </GoogleOAuthProvider>
+    <AppRoutes />
   </React.StrictMode>
 );
 
