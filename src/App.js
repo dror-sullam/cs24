33c77b9a678d2dfeb9f8e94ff17afc51db78dea7
Dror Sullam <dror.844@gmail.com>
2025-05-03 20:26:04 +0300

    Hotfix

--- src/App.js
+++ src/App.js
@@ -314,8 +314,8 @@ const App = () => {
   return (
     <NotificationProvider>
       <div className={`min-h-screen bg-gradient-to-b ${styles.bgGradient}`}>
-        { isDevMode && <Navbar courseType={courseType} /> }
-        <main className={`container mx-auto px-4 py-8 ${ isDevMode && 'pt-24' }`}>
+        <Navbar courseType={courseType} />
+        <main className={`container mx-auto px-4 py-8 pt-24`}>
           <AdminPanel user={user} />
           <div className="flex flex-col items-center mb-4">
             <h1 className={`text-5xl font-bold mb-4 text-center ${styles.textColor}`}>CS24</h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,26 +18,27 @@ const Navbar = ({ courseType }) => {
     <nav className="fixed top-0 w-full z-50 bg-white p-3 shadow-md flex items-center justify-between">
       <NavRightSection setIsOpen={setIsOpen} styles={styles} />
       <NavLeftSection styles={styles} />
-      <NavDropdownMenu isOpen={isOpen} styles={styles} />
+      {/* <NavDropdownMenu isOpen={isOpen} styles={styles} /> */}
     </nav>
   );
 };
 
 const NavRightSection = ({ setIsOpen, styles }) => {
   return (
-    <div className="flex items-center gap-6 lg:mr-4">
-      <motion.button
+    <div className="flex items-center gap-6 mr-4">
+      {/* Change to lg:mr-4 after enabling! */}
+      {/* <motion.button
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         className="block lg:hidden text-gray-950 text-2xl"
         onClick={() => setIsOpen((pv) => !pv)}
       >
         <FiMenu size={32}/>
-      </motion.button>
+      </motion.button> */}
       <InstituteLogo styles={styles} />
-      <NavbarLink text="ראשי" styles={styles} />
+      {/* <NavbarLink text="ראשי" styles={styles} />
       <NavbarLink text="מחשבון ציונים" styles={styles} />
-      <NavbarLink text="אודות" styles={styles} />
+      <NavbarLink text="אודות" styles={styles} /> */}
     </div>
   );
 };
@@ -60,9 +61,9 @@ const NavDropdownMenu = ({ isOpen, styles }) => {
       animate={isOpen ? "open" : "closed"}
       className="absolute p-5 bg-white shadow-lg left-0 right-0 top-full origin-top flex flex-col gap-4 lg:hidden"
     >
-      <DropdownMenuLink text="ראשי" styles={styles} />
+      {/* <DropdownMenuLink text="ראשי" styles={styles} />
       <DropdownMenuLink text="מחשבון ציונים" styles={styles} />
-      <DropdownMenuLink text="אודות" styles={styles} />
+      <DropdownMenuLink text="אודות" styles={styles} /> */}
     </motion.div>
   );
 };
